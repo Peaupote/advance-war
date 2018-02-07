ae8ea8df67d434f72f1dc2043426b250a6517f7e
Maxime Flin <maxime.flin@gmail.com>
2018-02-07 20:57:17 +0100

    revert

--- fr/main/model/buildings/Building.java
+++ fr/main/model/buildings/Building.java
@@ -2,9 +2,16 @@ package fr.main.model.buildings;
 
 import java.io.Serializable;
 
-public interface Building extends Serializable{
+public class Building implements Serializable, AbstractBuilding{
+	public final String name;
+	public final int defense;
 
-	public String toString();
-	public int getDefense();
+	public Building(String name, int defense) {
+		this.name = name;
+		this.defense = defense;
+	}
+	public String toString() {
+		return name;
+	}
 
 }

e1c7bbcaafe6d9d65146254d8e6df6d161ac6fc8
Maxime Flin <maxime.flin@gmail.com>
2018-02-21 23:21:22 +0100

    remove path cycle

--- fr/main/model/units/Path.java
+++ fr/main/model/units/Path.java
@@ -1,6 +1,7 @@
 package fr.main.model.units;
 
 import java.util.LinkedList;
+import java.awt.Point;
 import fr.main.model.Direction;
 
 public class Path extends LinkedList<Direction> {
@@ -17,9 +18,27 @@ public class Path extends LinkedList<Direction> {
   }
 
   @Override
-  public boolean add (Direction d) {
-    // TODO: do some check or change on the path here
-    return super.add(d);
+  public boolean add (Direction dir) {
+    // TODO: test if over path length and eval a new path
+    Point t = new Point (0, 0);
+    LinkedList<Point> pts = new LinkedList<>();
+    pts.add(t);
+
+    for (Direction d: this) {
+      t = (Point)t.clone();
+      d.move(t);
+      pts.add(t);
+    }
+
+    t = (Point)t.clone();
+    dir.move(t);
+    int i = pts.indexOf(t);
+    if (i != -1) {
+      for (int j = i; j < pts.size() - 1; j++) removeLast();
+      return true;
+    }
+
+    return super.add(dir);
   }
 
   public void apply () {

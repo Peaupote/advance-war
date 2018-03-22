e4deb8011be5b26cb6a734ed2e02642180863fea
Maxime Flin <maxime.flin@gmail.com>
2018-03-22 19:17:22 +0100

    Add icons in Terrain Panel

--- fr/main/view/interfaces/TerrainPanel.java
+++ fr/main/view/interfaces/TerrainPanel.java
@@ -11,6 +11,7 @@ import fr.main.model.Player;
 import fr.main.model.units.Unit;
 import fr.main.view.Position;
 import fr.main.view.render.terrains.TerrainRenderer;
+import fr.main.view.render.sprites.Sprite;
 
 public class TerrainPanel extends InterfaceUI {
 
@@ -19,6 +20,12 @@ public class TerrainPanel extends InterfaceUI {
   static final int WIDTH = 100, HEIGHT = 200, MARGIN = 10,
           HALFW = MainFrame.WIDTH / (2 * MainFrame.UNIT),
           HALFH = MainFrame.HEIGHT / (2 * MainFrame.UNIT);
+
+  private static Sprite sp = Sprite.get("./assets/small_sprites.png");
+  private static final Image lifeImage = sp.getImage(0, 8, 8, 8, 2),
+                             munitionsImage = sp.getImage(0, 16, 8, 7, 2),
+                             fuelImage = sp.getImage(0, 23, 8, 8, 2),
+                             moveImage = sp.getImage(0, 0, 8, 8, 2);
 
   boolean leftSide;
   int x, y;
@@ -52,22 +59,27 @@ public class TerrainPanel extends InterfaceUI {
     // Units info :
     AbstractUnit unit = world.getUnit(cursor.getX(), cursor.getY());
     if(world.isVisible(cursor.getX(), cursor.getY()) && unit != null) {
-      g.drawString(unit.getName(), x + 20, y + 100);
-      g.drawString(unit.getPlayer().name, x + 20, y + 120);
-      g.drawString(unit.getLife() + "/100", x + 20, y + 140);
-      g.drawString(unit.getMoveQuantity() + "/" + unit.getMaxMoveQuantity(), x + 20, y + 160);
+      g.drawString(unit.getName(), x + 15, y + 100);
+      g.drawString(unit.getPlayer().name, x + 15, y + 120);
+
+      g.drawImage(lifeImage, x + 15, y + 130, null);
+      g.drawString(unit.getLife() + "/100", x + 35, y + 140);
+
+      g.drawImage(moveImage, x + 15, y + 150, null);
+      g.drawString(unit.getMoveQuantity() + "/" + unit.getMaxMoveQuantity(), x + 35, y + 160);
           
       Unit.Fuel fuel = unit.getFuel();
-      g.drawString(fuel.getQuantity()+"/"+fuel.maximumQuantity, x + 20, y + 180);
+      g.drawImage(fuelImage, x + 15, y + 170, null);
+      g.drawString(fuel.getQuantity()+"/"+fuel.maximumQuantity, x + 35, y + 180);
     } else {
       AbstractBuilding building  = world.getBuilding(cursor.getX(), cursor.getY());
       if (building != null) {
-        g.drawString(building.getName(), x + 20, y + 100);
+        g.drawString(building.getName(), x + 15, y + 100);
         if (building instanceof OwnableBuilding) {
           Player p = ((OwnableBuilding)building).getOwner();
-          g.drawString(p == null ? "Neutral" : p.name, x + 20, y + 120);
+          g.drawString(p == null ? "Neutre" : p.name, x + 15, y + 120);
         }
-      } else g.drawString ("No Unit", x + 20, y + 100);
+      } else g.drawString ("No Unit", x + 15, y + 100);
     }
   }
 
